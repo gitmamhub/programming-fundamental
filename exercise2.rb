a8f8d92e4d8bfd410911a472af5d3ce3d3e84c77
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-04 15:42:41 -0400

    exercise 2 final

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+# calculate tip for 55 dollar meal
+
+
+
+
+puts "Tip can be 5% for my meal. #{55 * (5.0/100)}$"
+
+# converting integer in to string  and display
+puts" Tips can be " + 2.75.to_s + "after conversion "
+
+# string interpolation
+
+puts "the multiplication #{45628 * 7839}"
+
+# expression recharge
+
+puts (10 < 20) && (30 < 20 ) || !(10 ==11)
